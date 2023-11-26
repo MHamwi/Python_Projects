73dcf2a3b7e9de05760d2daf3390910a107922a0
Mohammad Hamwi <owmohammadwo@gmail.com>
2023-11-26 13:57:21 +0300

    new_version_of_guessing_game

--- Guessing_Game/Guessing_Game.py
+++ Guessing_Game/Guessing_Game.py
@@ -1,57 +1,61 @@
-from ast import IsNot
 import random
 
-from numpy import integer
-attempts_history= []
-attempt = 0
+attempts_history = []
 
-def compare_number(computer,player,attempt):
-    attempt+=1
+def compare_number(computer, player, attempt):
+    attempt += 1
+
     if player > computer:
-        try:
-            player= int(input(f'you have to guess lower number: '))
-            if player > 10 or player < 1:
-                raise ValueError
-        except ValueError:
-            print(input(f'you have to guess lower number: '))
-            player= int(input(f'you have to guess lower number: '))
+        while True:
+            try:
+                player = int(input('You have to guess a lower number: '))
+                if not 1 <= player <= 10:
+                    raise ValueError("Number must be between 1 and 10.")
+                break
+            except ValueError:
+                print("Invalid input. Please enter an integer between 1 and 10.")
+
     elif player < computer:
-        try:
-            player = int(input(f'you have to guess higher number: '))
-            if player > 10 or player< 1:
-                raise ValueError
-        except ValueError:
-            print(input(f'you have to guess higher number: '))
-            player = int(input(f'you have to guess higher number: '))
+        while True:
+            try:
+                player = int(input('You have to guess a higher number: '))
+                if not 1 <= player <= 10:
+                    raise ValueError("Number must be between 1 and 10.")
+                break
+            except ValueError:
+                print("Invalid input. Please enter an integer between 1 and 10.")
+
     else:
         return attempt
-    return compare_number(computer,player, attempt)
 
+    return compare_number(computer, player, attempt)
 
 def guessing_game():
-    print('welcome back, are you ready for hitting some heads?!')
-    players={'player':int,'computer':random.randint(1,10)}
-    #print(players)
-    user_input = input('are you sure that, do you want to playing this horror game? (yes/no): ').lower()
+    print('Welcome back! Are you ready for some fun?')
+    
+    players = {'player': 0, 'computer': random.randint(1, 10)}
+
+    user_input = input('Are you sure you want to play this game? (yes/no): ').lower()
+
     if user_input == 'yes':
-        try:
-            player_number = int(input('guess a number between 1 to 10: '))
-            players['player']= player_number
-            if player_number > 10 or player_number < 1:
-                raise ValueError
-        except ValueError:
-            print('you have to guess an integer number between 1 to 10')  
-            player_number = int(input('guess a number between 1 to 10: '))  
-            players['player']= player_number
-        while user_input =='yes':
-            final_score = compare_number(players['computer'],players['player'], attempt)
-            print(final_score)
-            if final_score :
-                print(f'Congratulations! it took from you {final_score} attempts!')
-                attempts_history.append(final_score)
-                print(f'the best score is: {min(attempts_history)} attempts')
-                user_input = 'no'
-            guessing_game()
-                
-if __name__=='__main__':
+        while user_input == 'yes':
+            try:
+                player_number = int(input('Guess a number between 1 and 10: '))
+                if not 1 <= player_number <= 10:
+                    raise ValueError("Number must be between 1 and 10.")
+                players['player'] = player_number
+                break
+            except ValueError:
+                print("Invalid input. Please enter an integer between 1 and 10.")
+
+        final_score = compare_number(players['computer'], players['player'], 0)
+        
+        print(final_score)
+
+        if final_score:
+            print(f'Congratulations! It took you {final_score} attempts!')
+            attempts_history.append(final_score)
+            print(f'The best score is: {min(attempts_history)} attempts')
+
+if __name__ == '__main__':
     guessing_game()
